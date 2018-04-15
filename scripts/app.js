ef4c3a91f603c6ec36d33d25760ec536690e5225
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-04-15 22:27:51 +0530

    Add inout validation and a search feature on the input

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,51 @@
+/*
+ *@defineExtenders defines custom string validations for the search box
+ *It currently defines one extender, the alphaNumeric
+ */
+const defineExtenders = function () {
+  /*
+   *@alphaNumeric is a knockout custom string validator
+   *It subscribes to updates on keystrokes to the viewModel's ui_query
+   *It regexp tests the string input for any invalid inputs
+   */
+  ko.extenders.alphaNumeric = function ( target, overrideMessage ) {
+    //add some sub-observables to our observable
+    target.hasError = ko.observable();
+    target.validationMessage = ko.observable();
+
+    //define a function to do validation
+    function validate( value ) {
+      //skip if null
+      if ( !value || value.length === 0 ) {
+        target.hasError( false );
+      } else {
+        const invalid = /[^a-zA-Z0-9]/.test( value );
+        target.hasError( invalid ? true : false );
+        target.validationMessage( invalid ? overrideMessage || "This field is required to contain only alphanumeric content" : "" );
+      }
+    }
+
+    //initial validation
+    validate( target() );
+
+    //validate whenever the value changes
+    target.subscribe( validate );
+
+    //return the original observable
+    return target;
+  };
+};
+/*
+ *@ViewModel is the data descriptor in the MV-VM knockout framework
+ *It defines all possible view bound and other properties of our representation
+ *of the app in terms of the data it abstracts
+ */
 const ViewModel = function () {
-  this.ui_query = ko.observable( '' );
+  defineExtenders();
+  this.ui_query = ko.observable( '' )
+    .extend( {
+      alphaNumeric: "Please input a combination of alphabets and numbers or just alphabets"
+    } );
   this.errorMsg = ko.observable( '' );
   this.venues = ko.observableArray();
   this.currentVenue = ko.observable( '' );
@@ -24,7 +70,30 @@ const ViewModel = function () {
           .errorOnFetch );
       } )
   } );
-  this.filteredVenues = '';
+  this.filteredVenues = ko.computed( () => {
+    if ( this.ui_query()
+      .length > 0 ) {
+      this.errorMsg( '' );
+      const SubString = this.ui_query()
+        .toLowerCase();
+      const venues = ko.observableArray();
+      for ( const venue of this.venues() ) {
+        if ( venue.name.toLowerCase()
+          .indexOf( SubString ) !== -1 ) {
+          venues.push( venue );
+        }
+      }
+      if ( venues()
+        .length > 0 ) {
+        return venues();
+      } else {
+        this.errorMsg( 'No Results' );
+      }
+    } else {
+      this.errorMsg( '' );
+      return this.venues();
+    }
+  } );
   this.fetchInfo = ( venue ) => {
     //Once you can get a premium foursquare account,
     //use this space to fetch venue details
